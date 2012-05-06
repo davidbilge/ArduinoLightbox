c2a7342dd05d9c1dc5c97abd5d49c0f3397a3898
David Bilge <david.bilge@gmail.com>
2012-05-06 22:15:11 +0200

    Added cycling menu

--- lightbox_test.ino
+++ lightbox_test.ino
@@ -12,6 +12,8 @@
 #define NUM_OF_LEDS (MAX_ROW+1)*(MAX_COL+1)
 
 #define PIC_LEN 1
+#define SELECTION_TIMEOUT 2000
+#define SELECTION_FLICKER 250
 
 #include "symbols.h"
 #include "picture.h"
@@ -22,7 +24,8 @@ Bounce bouncer = Bounce( PIN_BUTTON,5 );
 int current_picture = 0;
 Picture* pictures[PIC_LEN] = {new H_Lines()};
 
-int msecs = millis();
+long msecs = millis();
+long selection_timer = 0;
 
 void setup() {
   pinMode(PIN_BUTTON, INPUT);
@@ -49,21 +52,42 @@ void setup() {
   lc.clearDisplay(0);
 }
 
-void loop() {
+void loop() {  
+  static boolean display_menu = false;
+  
+  long msecs_old = msecs;
+  msecs = millis();
+  long delta = msecs - msecs_old;
+  
   bouncer.update();
   
   if (bouncer.fallingEdge()) {
     // Cycle picture
-    current_picture = (++current_picture) % PIC_LEN; 
+    current_picture = (++current_picture) % PIC_LEN;
+    lc.clearDisplay(0);
+    selection_timer = SELECTION_TIMEOUT;
+    display_menu = true;
+  } else if (bouncer.duration() == 3000) {
+    testCycle();
+    lc.clearDisplay(0);
   }
   
-  Picture* p = pictures[current_picture];
+  if (display_menu && selection_timer <= 0) {
+    display_menu = false;
+    lc.clearDisplay(0);
+  }
   
-  int msecs_old = msecs;
-  msecs = millis();
-  
-  if ((msecs - msecs_old) >= p->getDelay()) {
-    p->loop();
+  if (selection_timer <= 0) {
+    Picture* p = pictures[current_picture];
+    
+    if (delta >= p->getDelay()) {
+      p->loop();
+    }
+  } else {
+    // Display selection "menu"
+    displayMenu(current_picture);
+    
+    selection_timer -= delta;
   }
 }
 
@@ -77,6 +101,35 @@ void testCycle() {
   lightEvenCols();
   delay(1000);
   lightOddCols();
+}
+
+void displayMenu(int selectedItem) {
+  static boolean selection_mode = false;
+  static long old_millis = millis();
+  static int millis_since_last_change = SELECTION_FLICKER;
+  
+  long delta = millis() - old_millis;
+  old_millis = millis();
+  
+  millis_since_last_change -= delta;
+  if (millis_since_last_change <= 0) {
+    millis_since_last_change = SELECTION_FLICKER;
+    selectionMode = !selectionMode;
+  }
+    
+  for (int i=0; i<NUM_OF_LEDS; ++i) {
+    int row = i / (MAX_COL+1);
+    int col = i % (MAX_COL+1);
+    
+    
+    boolean light_mode;
+    if (i == selectedItem) {
+      light_mode = selection_mode;
+    } else {
+      light_mode = (i < PIC_LEN);
+    }
+    lc.setLed(0, row, col, light_mode);
+  }
 }
 
 void lightAll() {
